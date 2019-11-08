7d561604a748d24555b3a03c0524e3a60fb9c978
Liz P <parisielizabeth@gmail.com>
2019-11-08 06:45:14 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -40,12 +40,6 @@ class InteractiveRecord
     end
     values.join(", ")
   end
-  
-  def save
-    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
-    DB[:conn].execute(sql)
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
-  end
 
   
   
@@ -61,3 +55,16 @@ class InteractiveRecord
     DB[:conn].execute(sql)
   end
 end
+
+  def self.find_by(attr_hash)
+    value = attr_hash.values.first
+    formatted_value = value.class == Fixnum ? value : "'#{value}'"
+    sql = "SELECT * FROM #{self.table_name} WHERE {attribute_hash.keys.first} = #{formatted_value}"
+    DB[:conn].execute(sql)
+  end
+  
+    def save
+    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
+    DB[:conn].execute(sql)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+  end
