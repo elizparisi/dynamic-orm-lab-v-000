bf3bb99b2735b9412f9e3df2505e406b0d903348
Liz P <parisielizabeth@gmail.com>
2019-11-08 06:08:34 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -29,6 +29,10 @@ class InteractiveRecord
     self.class.table_name
   end
 
+  def col_names_for_insert
+    self.class.column_names.delete_if {|col| col == "id"}.join(", ")
+  end
+  
   def values_for_insert
     values = []
     self.class.column_names.each do |col_name|
@@ -37,9 +41,7 @@ class InteractiveRecord
     values.join(", ")
   end
 
-  def col_names_for_insert
-    self.class.column_names.delete_if {|col| col == "id"}.join(", ")
-  end
+  
   
     def self.find_by_name(name)
     sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
